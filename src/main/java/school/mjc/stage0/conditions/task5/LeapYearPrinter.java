a7c609c97b8813fbec832f18e6f6fe2255dc6e29
alikeli <eli-com2@yandex.ru>
2023-03-08 15:41:24 +0100

    done

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        if(year!=0)
+        {
+            if(year%400==0)
+                System.out.println("leap");
+            else  if(year%100==0)
+                System.out.println("not leap");
+            else if(year%4==0)
+                System.out.println("leap");
+            else
+                System.out.println(" not leap");
+        }
+        else
+            System.out.println("Year zero does not exist ");
     }
-}
+    }
+
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,29 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month < 1 || month > 12) {
+            System.out.println("wrong number!");
+        } else {
+
+
+            int numberOfDays;
+            switch (month) {
+                case 2:
+                    numberOfDays = 28;
+                    break;
+
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    numberOfDays = 30;
+                    break;
+                default:
+                    numberOfDays = 31;
+                    break;
+            }
+
+            System.out.println(numberOfDays);
+        }
     }
 }
